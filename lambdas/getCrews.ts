cdeb3b1dd514ecbf2440c2232591fdf0a5545703
Yuanzhe Yang <vinceHello@outlook.com>
2024-03-22 10:44:46 +0000

    Added new endpoint

---
+++ lambdas/getCrews.ts
@@ -0,0 +1,73 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbDocClient = createDDbDocClient();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+    try {
+
+
+        const parameters = event?.pathParameters;
+        const crewRole = parameters?.role ? parameters.role: undefined;
+        const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
+
+
+        let commandInput = {
+            TableName: process.env.TABLE_NAME,
+            KeyConditionExpression: "movieId = :movieId and crewRole = :crewRole",
+            ExpressionAttributeValues: {
+                ":movieId": movieId,
+                ":crewRole": crewRole,
+            },
+
+        };
+
+
+        const commandOutput = await ddbDocClient.send(new QueryCommand(commandInput));
+
+        if (!commandOutput.Items || commandOutput.Items.length === 0) {
+            return {
+                statusCode: 404,
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({ message: "No crew found for the given criteria" }),
+            };
+        }
+
+        const body = {
+            data: commandOutput.Items
+        };
+        return {
+            statusCode: 200,
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(body),
+        };
+    } catch (error: any) {
+        console.log(JSON.stringify(error));
+        return {
+            statusCode: 500,
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ error }),
+        };
+    }
+};
+
+function createDDbDocClient() {
+    const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+    const marshallOptions = {
+        convertEmptyValues: true,
+        removeUndefinedValues: true,
+        convertClassInstanceToMap: true,
+    };
+    const unmarshallOptions = {
+        wrapNumbers: false,
+    };
+    const translateConfig = { marshallOptions, unmarshallOptions };
+    return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
